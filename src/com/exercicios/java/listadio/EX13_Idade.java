47e6c1853c063a4d8df933f7aa67dc6730a80436
beatrizkloss <alannnamas@gmail.com>
2026-01-18 20:20:50 -0300

    exercicios completos de 13-14 da lista da dio

---
+++ src/com/exercicios/java/listadio/EX13_Idade.java
@@ -0,0 +1,20 @@
+package com.exercicios.java.listadio;
+
+import java.util.Scanner;
+
+public class EX13_Idade {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+
+        if(idade < 18 ){
+            System.out.println(nome + " você tem " + idade + " você é MENOR de idade");
+        }else {
+            System.out.println(nome + " você tem " + idade + " você é MAIOR de idade");
+        }
+    }
+}
---
+++ src/com/exercicios/java/listadio/EX14_Troca.java
@@ -0,0 +1,19 @@
+package com.exercicios.java.listadio;
+
+import java.util.Scanner;
+
+public class EX14_Troca {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        int A = sc.nextInt();
+        System.out.println("Digite o segundo nÚmero");
+        int B = sc.nextInt();
+        int C = A;
+        A = B;
+        B = C;
+
+        System.out.println("O primeiro número foi trocado pelo segundo: " + A);
+        System.out.println("O segundo número foi trocado pelo primeiro: " + B);
+    }
+}
